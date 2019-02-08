55b47b1f9f8a7ef599d05722e8ce79626c48b693
Lemley <gavin.lemley@dec.ny.gov>
2019-02-08 16:46:15 -0500

    Added data source.

--- getting.started.R
+++ getting.started.R
@@ -20,10 +20,12 @@
 
 #first load the data file
 
-data<-read.csv("sections/data/projectData/LCI.2018/2018data.csv")
+# data<-read.csv("sections/data/projectData/LCI.2018/2018data.csv")
+data<-read.csv("sections/data/projectData/Streams/2017_minnewaska/Minnewaska_chem_2017_raw.csv")
 
 #This file is a list of lab errors extracted from the ALS PDF reports on the first page of "Narrative Documents". See General Chemistry and Metals (not always present)
-errors<-read.csv("sections/data/projectData/LCI.2018/laberrors.csv")
+# errors<-read.csv("sections/data/projectData/LCI.2018/laberrors.csv")
+errors<-read.csv("sections/data/projectData/Streams/2017_minnewaska/laberrors.csv")
 
 
 #truncate the input file to only the necessary fields
@@ -40,7 +42,8 @@ rmarkdown::render("QAQC.Rmd", params = inputs)
 
 #write the data output
 
-write.csv(forprint,file="sections/data/projectData/Streams/2018_Ramapo/2018_Ramapo_qaqcd_1-23-19.csv",row.names = FALSE)
+# write.csv(forprint,file="sections/data/projectData/Streams/2018_Ramapo/2018_Ramapo_qaqcd_1-23-19.csv",row.names = FALSE)
+write.csv(forprint,file="sections/data/projectData/Streams/2017_minnewaska/Minnewaska_2017_chem_qaqcd_2-8-19_test_2.csv",row.names = FALSE)
 
 rm(forprint)
 
