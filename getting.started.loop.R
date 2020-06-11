ca6f548a4f288e6574ee2503c1419e63ae0c8587
Lemley <gavin.lemley@dec.ny.gov>
2020-06-11 19:33:30 -0400

    Loop updates for rerunning 2018, 2019 streams data. Fix precision plot and make backup.

--- getting.started.loop.R
+++ getting.started.loop.R
@@ -30,16 +30,16 @@ library(furrr)
 project.dir <- "sections/data/projectData/Streams/"
 
 # #2019
-# input.dir <- "2019/all/"
-# input.data <- "2019_chem_preqaqc_ALL-SMAS_complete_2020-05-11.csv"
-# proj.list.file <- "ALS_project_list_FINAL_NYSDEC 2019_2019-12-04.xlsx"
-# proj.year <- "2019"
+input.dir <- "2019/all/"
+input.data <- "2019_chem_preqaqc_ALL-SMAS_complete_2020-05-11.csv"
+proj.list.file <- "ALS_project_list_FINAL_NYSDEC 2019_2019-12-04.xlsx"
+proj.year <- "2019"
 
 #2018
-input.dir <- "2018/all/"
-input.data <- "2018_chem_preqaqc_ALL_SMAS_complete_2020-05-08.csv"
-proj.list.file <- "ALS_2018_project_list_FINAL.xlsx"
-proj.year <- "2018"
+# input.dir <- "2018/all/"
+# input.data <- "2018_chem_preqaqc_ALL_SMAS_complete_2020-05-08.csv"
+# proj.list.file <- "ALS_2018_project_list_FINAL.xlsx"
+# proj.year <- "2018"
 
 
 
@@ -50,16 +50,22 @@ proj.year <- "2018"
 proj.list <- readxl::read_excel(paste0(project.dir, input.dir, proj.list.file)) %>% 
   select("Folder#", "project_QAQC") %>% 
   dplyr::rename("sample_delivery_group" = "Folder#") %>% 
-  mutate(project_QAQC = str_replace_all(project_QAQC, " ", "_")) %>%
-  filter(project_QAQC != "Quassaic_Creek_RAS") %>% 
-  filter(project_QAQC != "Duane_Lake_RAS") %>% 
-  filter(project_QAQC != "Ausable_RAS")
+  mutate(project_QAQC = str_replace_all(project_QAQC, " - ", "_")) %>% 
+  mutate(project_QAQC = str_replace_all(project_QAQC, " ", "_")) %>% 
+  mutate(project_QAQC = toupper(project_QAQC)) 
+  # filter(project_QAQC != "Quassaic_Creek_RAS") %>%
+  # filter(project_QAQC != "Duane_Lake_RAS") %>%
+  # filter(project_QAQC != "Ausable_RAS")
   
 
 data.proj <- read.csv(paste0(project.dir, input.dir, input.data), colClasses = c(fraction="character"), stringsAsFactors = FALSE) %>% 
   left_join(proj.list, by = "sample_delivery_group") %>% 
   dplyr::select(project_QAQC, everything()) %>%
-  filter(!is.na(project_QAQC))
+  filter(!is.na(project_QAQC)) %>% 
+  mutate(chemical_name = ifelse(chemical_name %in% "magnesium", "Magnesium", chemical_name))
+
+data.proj.lowermg <- data.proj %>% 
+  filter(chemical_name %in% "magnesium")
 
 # Look for SDGs with no project name match
 # data.proj.na <- data.proj %>%
@@ -68,11 +74,15 @@ data.proj <- read.csv(paste0(project.dir, input.dir, input.data), colClasses = c
 
 proj.names <- unique(data.proj$project_QAQC)
 
+# look for missing sdgs
+missing.sdgs <- anti_join(proj.list, data.proj, by = "sample_delivery_group")
+
 # For troubleshooting single project
-# name.i <- proj.names[18]
+# name.i <- proj.names[10,15,17,22]
+name.i <- proj.names[22]
 
 future::plan(multiprocess)
-furrr::future_map(proj.names, .progress = TRUE, function(name.i){
+furrr::future_map(proj.names[22], .progress = TRUE, function(name.i){
 # lapply(proj.names[1:2], function(name.i){
     
   print(name.i)
